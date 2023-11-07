fdb510e1139c7d775ea53c19f18cb4821a3098c2
Lindsay Mergy <lindsay.mergy@thearchitects.io>
2023-11-06 17:13:22 -0800

    feat: video header mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="videoContainer">
+        <div className="videoBackground">
+          <iframe
+            title="YouTube Video"
+            width="100%"
+            height="90%" /* Adjust height to cut off the top 10% of the video */
+            src="https://www.youtube.com/embed/hQItqD6ERLM?autoplay=1&controls=0&mute=1&loop=1"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+          <div className="textOverlay">
+            <h2>Discover Tranquility</h2>
+          </div>
+        </div>
+      </div>
+      <div className="contentContainer">
+        <div className="content blue">
+          <h2>content here</h2>
+          <p>Nestled beneath the towering pine trees, our luxurious safari tents offer a serene escape into the heart of nature.</p>
+          <p>Embrace the cozy ambience of a wilderness cabin, where stargazing by the campfire is a nightly ritual.</p>
+          <p>Wander through the enchanted forest and unwind in a riverside yurt, where the tranquil sounds of flowing water lull you to sleep.</p>
+        </div>
+        <div className="content green">
+          <h2>Book your stay at our oasis</h2>
+          <p>Embrace the cozy ambience of a wilderness cabin, where stargazing by the campfire is a nightly ritual.</p>
+          <p><a href="https://www.airbnb.com/rooms/822425509654829635">Enjoy a yurt</a></p>
+          <p>Wander through the enchanted forest and unwind in a riverside yurt, where the tranquil sounds of flowing water lull you to sleep.</p>
+          <p><a href="https://www.airbnb.com/rooms/822425509654829635">Have a blast in a dome</a></p>
+        </div>
+        <div className="content blue">
+          <h2>content here</h2>
+          <p>Nestled beneath the towering pine trees, our luxurious safari tents offer a serene escape into the heart of nature.</p>
+          <p>Embrace the cozy ambience of a wilderness cabin, where stargazing by the campfire is a nightly ritual.</p>
+          <p>Wander through the enchanted forest and unwind in a riverside yurt, where the tranquil sounds of flowing water lull you to sleep.</p>
+        </div>
+      </div>
     </div>
   );
 }
